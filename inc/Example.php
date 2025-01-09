62839f53d070221b18b83696aa7734a30bb602b0
MusTangss <mustaphatoubaline0@gmail.com>
2025-01-09 14:04:31 +0100

    Update Example.php

--- inc/Example.php
+++ inc/Example.php
@@ -5,7 +5,7 @@ class Example {
 		The reason for this class to exist is to act as documentation
 		for passing multiple directories to PHPStan.
 		
-		Have a great day!
+		Have a great day!!
 		STR;
 	}
 }
